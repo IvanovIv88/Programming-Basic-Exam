06e74679436baaa0e9557cf174f9c916691d87d7
IvanovIv88 <l0k3r_@abv.bg>
2018-03-10 22:32:31 +0200

    New exam

---
+++ Test/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int guests = int.Parse(Console.ReadLine());
+            int budget = int.Parse(Console.ReadLine());
+
+            int totalMoney = guests * 20;
+
+            if (totalMoney < budget)
+            {
+                int remainingMoney = budget - totalMoney;
+                double moneyForFireworks = remainingMoney - (remainingMoney * 0.40);
+                double moneyForDonation = remainingMoney - moneyForFireworks;
+
+                Console.WriteLine($"Yes! {moneyForFireworks:f0} lv are for fireworks and {moneyForDonation:f0} lv are for donation.");
+            }
+            else
+            {
+                Console.WriteLine($"They won't have enough money to pay the covert. They will need {(totalMoney - budget):f0} lv more.");
+            }
+        }
+    }
+}
